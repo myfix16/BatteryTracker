915190971f0b1966bd9edb4b55213d07d7c086b8
Frank Mao <myfix16@outlook.com>
2023-01-10 04:46:44 +0800

    migrate to Windows App SDK 1.2

---
+++ src/App.xaml.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Input;
+using LaunchActivatedEventArgs = Microsoft.UI.Xaml.LaunchActivatedEventArgs;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace BatteryTracker
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    public partial class App : Application
+    {
+        static BatteryIcon? _batteryIcon;
+
+        #region Constructors
+
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Event Handlers
+
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            InitializeTrayIcon();
+        }
+
+        private void InitializeTrayIcon()
+        {
+            var exitApplicationCommand = (XamlUICommand)Resources["ExitApplicationCommand"];
+            exitApplicationCommand.ExecuteRequested += ExitApplicationCommand_ExecuteRequested;
+
+            var pushNotificationCommand = (XamlUICommand)Resources["SwitchNotificationCommand"];
+            pushNotificationCommand.ExecuteRequested += (_, o) => _batteryIcon?.PushNotification("", "");
+
+            _batteryIcon = new BatteryIcon();
+            _batteryIcon.Init(Resources);
+        }
+
+        private void ExitApplicationCommand_ExecuteRequested(object? _, ExecuteRequestedEventArgs args)
+        {
+            _batteryIcon?.Dispose();
+        }
+
+        #endregion
+    }
+}
---
+++ src/BatteryIcon.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using CommunityToolkit.WinUI.UI.Helpers;
+using H.NotifyIcon;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.Windows.AppNotifications;
+using Microsoft.Windows.AppNotifications.Builder;
+using Microsoft.Windows.System.Power;
+using Brush = Microsoft.UI.Xaml.Media.Brush;
+using Color = Windows.UI.Color;
+
+namespace BatteryTracker
+{
+    internal class BatteryIcon : IDisposable
+    {
+        private static readonly Brush White = new SolidColorBrush(Color.FromArgb(255, 255, 255, 255));
+        private static readonly Brush Black = new SolidColorBrush(Color.FromArgb(255, 0, 0, 0));
+
+        private TaskbarIcon? _trayIcon;
+        private ThemeListener? _themeListener;
+
+        public void Init(ResourceDictionary resources)
+        {
+            _trayIcon = (TaskbarIcon)resources["TrayIcon"];
+            _trayIcon.ForceCreate();
+
+            // register power events
+            PowerManager.RemainingChargePercentChanged += UpdateTrayIconPercent;
+
+            // register theme events
+            _themeListener ??= new ThemeListener();
+            _themeListener.ThemeChanged += UpdateTrayIconTheme;
+        }
+
+        ~BatteryIcon()
+        {
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            PowerManager.RemainingChargePercentChanged -= UpdateTrayIconPercent;
+            if (_themeListener != null) _themeListener.ThemeChanged -= UpdateTrayIconTheme;
+            _trayIcon?.Dispose();
+        }
+
+        internal void PushNotification(string title, string message)
+        {
+            var builder = new AppNotificationBuilder()
+                .AddText("Send a message.")
+                .AddTextBox("textBox");
+
+            var notificationManager = AppNotificationManager.Default;
+            notificationManager.Show(builder.BuildNotification());
+        }
+
+        private void UpdateTrayIconPercent(object? _, object eventArg)
+        {
+            if (_trayIcon is null) return;
+            int chargePercent = PowerManager.RemainingChargePercent;
+            _trayIcon.GeneratedIcon ??= new GeneratedIcon();
+            _trayIcon.GeneratedIcon.Text = chargePercent == 100 ? "F" : $"{chargePercent}%";
+            PushNotification("", "");
+        }
+
+        private void UpdateTrayIconTheme(ThemeListener sender)
+        {
+            if (_trayIcon is null) return;
+            _trayIcon.GeneratedIcon ??= new GeneratedIcon();
+            _trayIcon.GeneratedIcon.Foreground = sender.CurrentTheme == ApplicationTheme.Dark
+                ? White : Black;
+        }
+    }
+}
